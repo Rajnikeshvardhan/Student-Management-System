9fa507bab6edb18020fe6243ed091d3b16ad4fc8
RajNikeshVardhan <nikesh9701@gmail.com>
2025-06-21 18:59:59 +0530

    Summary

--- Student Backend/StudentManagementSystem/src/main/java/com/example/StudentManagementSystem/Controller/StudentController.java
+++ Student Backend/StudentManagementSystem/src/main/java/com/example/StudentManagementSystem/Controller/StudentController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,25 +29,22 @@ public class StudentController {
 	
 	@PostMapping("/students")
 	public student post(@Valid @RequestBody student stud) {
-		// TODO Auto-generated method stub
 		return serv.save(stud);
 	}
 
 	@GetMapping("/students")
 	public List<student> getAll() {
-		// TODO Auto-generated method stub
+
 		return serv.getAll();
 	}
 
 	@GetMapping("/students/{id}")
 	public Optional<student> getById(@PathVariable("id") int id1) {
-		// TODO Auto-generated method stub
 		return serv.getById(id1);
 	}
 
 	@PutMapping("/students/{id}")
 	public student put(@PathVariable("id") student id1,@Valid @RequestBody student stud) {
-		// TODO Auto-generated method stub
 		return serv.save(stud);
 	}
 	
@@ -56,14 +52,13 @@ public class StudentController {
 
 	@DeleteMapping("/students/{id}")
 	public void deleteById(@PathVariable("id") int id1) {
-		// TODO Auto-generated method stub
 		serv.deleteById(id1);
 		
 	}
 
 	@GetMapping("/search")
 	public List<student> findByCourse(@RequestParam("course") String course1) {
-		// TODO Auto-generated method stub
+		
 		return serv.findByCourse(course1);
 	}
 
